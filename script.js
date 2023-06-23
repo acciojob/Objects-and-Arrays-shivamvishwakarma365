75e8028023b0c3bb3caa81ef71fe3afe5f9aef7c
shivamvishwakarma365 <78745548+shivamvishwakarma365@users.noreply.github.com>
2023-06-23 22:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
+var team=players;
+var team1=players.slice();
 
 const person = {
   name: "John Doe",
   age: 80,
 };
-
+var cap1={...person};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
